cf6813d315f0264907f32f56a8b5a774ad13f84b
Richard Harris <120225671@umail.ucc.ie>
2021-04-26 16:02:10 +0100

    2.1

---
+++ 2.1.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main2_1() {
+	var x int
+	var y int
+
+	x = 1
+	y = 2
+
+	fmt.Printf("x=%v, type of %T\n", x, y)
+	fmt.Printf("y=%v, type of %T\n", y, y)
+
+	var mean int
+	mean = (x + y) / 2
+	fmt.Printf("Result: %v, type of %T\n", mean, mean)
+
+}
--- HelloWorld.go
+++ HelloWorld.go
@@ -8,5 +8,6 @@ func main() {
 	fmt.Println("Hello World!!")
 	fmt.Println("Hello World2")
 	fmt.Println("Hello World3")
+	main2_1()
 
 }
