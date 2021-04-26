25e7098bb8cc61e69af09861bc688371e699bc0c
Richard Harris <120225671@umail.ucc.ie>
2021-04-26 16:11:55 +0100

    2.1 Number 3

--- HelloWorld.go
+++ HelloWorld.go
@@ -6,12 +6,13 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello World!!")
+	Func2_1()
 
 }
 
 func Func2_1() {
-	var x int
-	var y int
+	var x float64
+	var y float64
 
 	x = 1
 	y = 2
@@ -19,8 +20,8 @@ func Func2_1() {
 	fmt.Printf("x=%v, type of %T\n", x, y)
 	fmt.Printf("y=%v, type of %T\n", y, y)
 
-	var mean int
-	mean = (x + y) / 2
+	var mean float64
+	mean = (x + y) / 2.0
 	fmt.Printf("Result: %v, type of %T\n", mean, mean)
 
 }
